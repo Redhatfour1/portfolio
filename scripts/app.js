6ca62187c1111b7a91d3c8d1f7e96a3444f16607
Redhatfour1 <yaloumessenger@yahoo.com>
2017-04-04 13:29:42 -0700

    worked on adding array. aorked on more css. worked on organizing my files. data js.

---
+++ scripts/app.js
@@ -0,0 +1,43 @@
+'use strict'
+
+var portfolioTopics = []
+var portfolio = [];
+console.log(portfolio);
+//using a constructor function to create portfolio objects
+function workExperience (jobName, jobTitle, jobDiscription){
+  this.jobName = jobName;
+  this.jobTitle = jobTitle;
+  this.jobDiscription = jobDiscription;
+  this.jobAchievments = jobAchievments;
+  portfolio.push(this);
+}
+//this IIFE creates the topics objects upon loading
+// (function() {
+//   for (var i = 0; i < portfolioTopics.length; i++) {
+//     new Topics (portfolioTopics[i]);
+//   }
+//   console.log(portfolioTopics);
+// })();
+Topics.prototype.toHtml = function() {
+  var $newTopic = $('topic.template').clone();
+
+  $newTopic.find('h1').html(this.workExperience);
+  $newTopic.find('a').attr('href', this.accomplishments);
+  $newTopic.find('address').html(this.projects);
+  $newTopic.find('section').html(this.hobbies);
+
+  console.log($newTopic);
+
+//   $newTopic.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+//   $newTopic.append('<hr>');
+//   return $newTopic;
+// };
+
+rawData.forEach(function(topicObject) {
+  // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
+  topics.push(new Article(topicObject));
+});
+
+topics.forEach(function(a) {
+  $('#topics').append(a.toHtml());
+});
---
+++ scripts/data.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var blogData = [
+  {
+    jobName: 'A&R Solar',
+    jobTitle: 'Solar Sales Representative',
+    jobDiscription: '<p>I scheduled and led onsite solar evaluations for prospective residential and commercial clients.
+                    Based on company research I presented American made, in-state, and imported panels as well as inverters and components thus
+                    leveraging available utility savings, incentives, rebates and deduction programs to close deals.</p>'
+    jobAchievments: 'Closed over $1.5 million in revenue with 46 new clients and nearly 300 kilowatts in 16 months'
+  },
+  {
+    jobName: 'Mpower Media',
+    jobTitle: 'Sales Lead and Operations Manager',
+    jobDiscription: '<p>Managed the NW Regional office overseeing 45 employees, major company functions and operations.
+                    Sold pre-produced media products to customers throughout the US and purchased media time.
+                    Negotiated media packages to secure advantages and favorable rates for clients in a variety of business segments.
+                    Established and maintained strong working relationships with media-channel sales representatives and vendors.
+                    Secured purchase related legal compliance, including affidavits and resolution of discrepancies.</p>'
+    jobAchievments: 'Re-negotiated a yearly placement of $1.2 million of media on a month basis for half the price'
+  },
+  {
+    jobName: 'Berndhart Construction',
+    jobTitle: 'Project Manager',
+    jobDiscription: '<p>I led sales and business development programs to a number of large and medium sized accounts.
+                    I also forecast sales revenue, volumes and profit for the company.
+                    Reviewed product pricing and gross margin goals to increase sales and return on investment.</p>',
+    jobAchievments: 'Active member of the sales team that closed a multi-million dollar deal with Expedia.com'
+  },
+];
