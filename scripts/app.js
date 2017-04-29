c327d548de01fdf684928bb5f8698ac64b08658e
Redhatfour1 <yaloumessenger@yahoo.com>
2017-04-29 16:54:39 -0700

    still working on getting json functional

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@
 
 function Topic (blogDataObj){
   this.myCategory = blogDataObj.myCategory;
-  // this.projects = blogDataObj.projects;
+  this.projects = blogDataObj.projects;
   this.tvShows = blogDataObj.tvShows;
   this.hobby = blogDataObj.hobby;
   this.projectName = blogDataObj.projectName;
@@ -35,11 +35,11 @@ Topic.fetchAll = function() {
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
-    Topic.loadAll(JSON.parse(localStorage.blogData)); //TODO: What do we pass in to loadAll()?
+    Topic.loadAll(JSON.parse(localStorage.blogData)); //What do we pass in to loadAll()?
     blogView.initIndexPage();
-    //TODO: What method do we call to render the index page?
+    //What method do we call to render the index page?
   } else {
-    // TODO: When we don't already have the rawData,
+    // When we don't already have the rawData,
     // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
